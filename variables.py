6cecfa9dd707493e5d2eb4abb3d9ba742cc4309e
namika-codes <namokain07@gmail.com>
2026-07-07 20:31:38 +0530

    Add variables.py

---
+++ variables.py
@@ -0,0 +1,27 @@
+# Variables in Python
+# First Python program created during my BCA.
+
+name = "Namika"
+age = 19
+course = "BCA"
+
+print("Name:", name)
+print("Age:", age)
+print("Course:", course)
+
+# String
+name = "Namika"
+
+# Integer
+age = 19
+
+# Float
+percentage = 91.5
+
+# Boolean
+is_student = True
+
+print(name)
+print(age)
+print(percentage)
+print(is_student)
